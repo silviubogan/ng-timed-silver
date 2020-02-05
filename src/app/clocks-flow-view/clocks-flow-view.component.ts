92b38317f2a57554b328e87e0cde0bcdce2e41ef
Silviu Bogan <silviubogan@gmail.com>
2020-02-05 12:55:25 +0200

    More work

--- src/app/clocks-flow-view/clocks-flow-view.component.ts
+++ src/app/clocks-flow-view/clocks-flow-view.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { Clock } from '../clock';
 
+import { CLOCKS } from '../mock-clocks';
+
 @Component({
   selector: 'app-clocks-flow-view',
   templateUrl: './clocks-flow-view.component.html',
@@ -12,15 +14,7 @@ export class ClocksFlowViewComponent implements OnInit {
   clocks : Clock[] = [];
 
   constructor() {
-    let c1 : Clock = new Clock();
-    c1.id = 1;
-    c1.tag = "My Timer";
-
-    let c2 : Clock = new Clock();
-    c2.id = 2;
-    c2.tag = "My Stopwatch";
-
-    this.clocks.push(c1, c2);
+    this.clocks.push(...CLOCKS);
   }
 
   ngOnInit() {
---
+++ src/app/mock-clocks.ts
@@ -0,0 +1,6 @@
+import { Clock } from './clock';
+
+export const CLOCKS: Clock[] = [
+    { id: 1, tag: "My Timer" },
+    { id: 2, tag: "My Stopwatch" }
+];
